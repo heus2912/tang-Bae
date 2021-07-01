26deda9b7451e3febc7ffa63dc6ec435fb4cba67
heus2912 <86777437+heus2912@users.noreply.github.com>
2021-07-01 17:43:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: `Tớ có điều này muốn hỏiii`,
+    btnIntro: ':3',
+    title: 'b có yêu tớ khônggggg 😙',
+    desc: 'chọn phương án đi nàooo ',
+    btnYes: 'Có <33',
     btnNo: 'Không. Còn lâu',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
